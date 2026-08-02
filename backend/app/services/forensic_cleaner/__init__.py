428420a7640d496a0fa50dd7dd6a23d5a3627fc6
nazicenn <nazfc7@gmail.com>
2026-08-02 18:33:58 +0300

    chore: add AdversarialCleaner to exports

--- backend/app/services/forensic_cleaner/__init__.py
+++ backend/app/services/forensic_cleaner/__init__.py
@@ -3,11 +3,13 @@ from app.services.forensic_cleaner.frequency_cleaner import FrequencyCleaner
 from app.services.forensic_cleaner.prnu_cleaner import PRNUCleaner
 from app.services.forensic_cleaner.fingerprint_cleaner import FingerprintCleaner
 from app.services.forensic_cleaner.compression_cleaner import CompressionCleaner
+from app.services.forensic_cleaner.adversarial_cleaner import AdversarialCleaner
 
 __all__ = [
     "MetadataCleaner",
     "FrequencyCleaner",
     "PRNUCleaner",
     "FingerprintCleaner",
-    "CompressionCleaner"
+    "CompressionCleaner",
+    "AdversarialCleaner"
 ]
