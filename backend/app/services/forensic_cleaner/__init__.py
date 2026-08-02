2f244d04c2b3f19e66470320496e2a2b11e53548
nazicenn <nazfc7@gmail.com>
2026-08-02 17:24:08 +0300

    chore: add FingerprintCleaner to exports

--- backend/app/services/forensic_cleaner/__init__.py
+++ backend/app/services/forensic_cleaner/__init__.py
@@ -1,5 +1,6 @@
 from app.services.forensic_cleaner.metadata_cleaner import MetadataCleaner
 from app.services.forensic_cleaner.frequency_cleaner import FrequencyCleaner
 from app.services.forensic_cleaner.prnu_cleaner import PRNUCleaner
+from app.services.forensic_cleaner.fingerprint_cleaner import FingerprintCleaner
 
-__all__ = ["MetadataCleaner", "FrequencyCleaner", "PRNUCleaner"]
+__all__ = ["MetadataCleaner", "FrequencyCleaner", "PRNUCleaner", "FingerprintCleaner"]
