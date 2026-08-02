6ee3f030038b7979f3b8de700684fc08f144949f
nazicenn <nazfc7@gmail.com>
2026-08-02 17:22:48 +0300

    chore: add PRNUCleaner to exports

--- backend/app/services/forensic_cleaner/__init__.py
+++ backend/app/services/forensic_cleaner/__init__.py
@@ -1,4 +1,5 @@
 from app.services.forensic_cleaner.metadata_cleaner import MetadataCleaner
 from app.services.forensic_cleaner.frequency_cleaner import FrequencyCleaner
+from app.services.forensic_cleaner.prnu_cleaner import PRNUCleaner
 
-__all__ = ["MetadataCleaner", "FrequencyCleaner"]
+__all__ = ["MetadataCleaner", "FrequencyCleaner", "PRNUCleaner"]
