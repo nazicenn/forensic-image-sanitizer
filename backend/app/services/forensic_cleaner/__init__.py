6815b4605322ace8ad70edcfde804b7b389191f1
nazicenn <nazfc7@gmail.com>
2026-08-02 18:37:38 +0300

    chore: add EnsembleCleaner to exports

--- backend/app/services/forensic_cleaner/__init__.py
+++ backend/app/services/forensic_cleaner/__init__.py
@@ -4,6 +4,7 @@ from app.services.forensic_cleaner.prnu_cleaner import PRNUCleaner
 from app.services.forensic_cleaner.fingerprint_cleaner import FingerprintCleaner
 from app.services.forensic_cleaner.compression_cleaner import CompressionCleaner
 from app.services.forensic_cleaner.adversarial_cleaner import AdversarialCleaner
+from app.services.forensic_cleaner.ensemble_cleaner import EnsembleCleaner
 
 __all__ = [
     "MetadataCleaner",
@@ -11,5 +12,6 @@ __all__ = [
     "PRNUCleaner",
     "FingerprintCleaner",
     "CompressionCleaner",
-    "AdversarialCleaner"
+    "AdversarialCleaner",
+    "EnsembleCleaner"
 ]
